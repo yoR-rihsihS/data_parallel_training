8023d6172ee44fc4b5a3ef18020e25eb6372daec
Shishir Roy <srshishirroy@gmail.com>
2025-04-09 15:30:28 +0530

    uploaded training scripts

--- train_ring_all_reduce.py
+++ train_ring_all_reduce.py
@@ -1,4 +1,5 @@
 import time
+import sys
 import torch
 import pickle
 import multiprocessing as mp
@@ -21,7 +22,7 @@ def save_file(object, path):
 def load_file(path):
     with open(path, 'rb') as file:
         return pickle.load(file)
-
+    
 def test(model, test_loader, loss_fn, device):
     correct = 0
     total = 0
@@ -44,10 +45,14 @@ def test(model, test_loader, loss_fn, device):
 
     return test_loss / num_batches, 100 * correct / total
 
-def train(process_id, queue_to_master, queue_from_master, train_loader, test_loader, state_init, num_epochs, metrics_queue, device, return_queue):
-    history = {'train_loss' : [], 'test_loss' : [], 'train_acc' : [], 'test_acc' : [], 'time' : []}
-    print("Process", process_id, "is using", device)
 
+def train(process_id, queues, train_loader, test_loader, state_init, num_epochs, metrics_queue, device, return_queue):
+    my_queue = queues[process_id]                           # Receive from previous process
+    send_queue = queues[(process_id + 1) % NUM_PROCESS]     # Send to next process
+    history = {'train_loss' : [], 'test_loss' : [], 'train_acc' : [], 'test_acc' : [], 'time' : []}
+    print("Process", process_id, "is using", device, flush=True)
+    sys.stdout.flush()
+    
     model = LeNet5(num_classes=10, conv_type='normal')
     model.load_state_dict(state_init)
     model = model.to(device)
@@ -79,26 +84,37 @@ def train(process_id, queue_to_master, queue_from_master, train_loader, test_loa
             loss.backward()
 
             grad_list = [param.grad.clone().cpu() for param in model.parameters()]
-            queue_to_master.put(grad_list)
+            flat_grads = torch.cat([grad.view(-1) for grad in grad_list])
 
-            if process_id == 0:
-                grad_lists = []
-                for i in range(NUM_PROCESS):
-                    grad_lists.append(queue_to_master.get())
+            chunk_size = flat_grads.size(0) // NUM_PROCESS
+            chunks = [flat_grads[i * chunk_size:(i + 1) * chunk_size].clone() for i in range(NUM_PROCESS - 1)]
+            chunks.append(flat_grads[(NUM_PROCESS - 1) * chunk_size:].clone())
 
-                sum_grad_list = []
-                for grads in zip(*grad_lists):
-                    sum_grad = sum(grads)
-                    sum_grad_list.append(sum_grad)
+            # Reduce-Scatter phase
+            for step in range(NUM_PROCESS - 1):
+                send_idx = (process_id - step) % NUM_PROCESS
+                send_data = chunks[send_idx].clone()
+                send_queue.put(send_data)
+                temp = my_queue.get()
+                recv_idx = (process_id - step - 1) % NUM_PROCESS
+                chunks[recv_idx] += temp
 
-                for i in range(NUM_PROCESS):
-                    queue_from_master.put(sum_grad_list)
+            # All-Gather phase
+            for step in range(NUM_PROCESS - 1):
+                send_idx = (process_id - step + 1) % NUM_PROCESS
+                send_data = chunks[send_idx].clone()
+                send_queue.put(send_data)
+                recv_idx = (process_id - step) % NUM_PROCESS
+                chunks[recv_idx] = my_queue.get()
 
-            sum_grad_list = queue_from_master.get()
-            average_grad_list = [sum_grad / NUM_PROCESS for sum_grad in sum_grad_list]
+            summed_flat_grads = torch.cat(chunks)
+            avg_flat_grads = summed_flat_grads / NUM_PROCESS
+
+            split_sizes = [g.numel() for g in grad_list]
+            average_grad_list = torch.split(avg_flat_grads, split_sizes)
 
             for param, avg_grad in zip(model.parameters(), average_grad_list):
-                param.grad = avg_grad.to(device)
+                param.grad = avg_grad.view(param.shape).to(device)
             optimizer.step()
         
         local_metrics = (train_loss, correct, num_batches, samples)
@@ -108,10 +124,12 @@ def train(process_id, queue_to_master, queue_from_master, train_loader, test_loa
             all_metrics = []
             for i in range(NUM_PROCESS):
                 all_metrics.append(metrics_queue.get())
+
+            # this is the right place to capture end time, after all processes send their metrics
             end = time.time()
 
-            total_loss = sum([metric[0] for metric in all_metrics])
-            total_correct = sum([metric[1] for metric in all_metrics])
+            total_loss = sum(metric[0] for metric in all_metrics)
+            total_correct = sum(metric[1] for metric in all_metrics)
             total_batches = sum([metric[2] for metric in all_metrics])
             total_samples = sum([metric[3] for metric in all_metrics])
 
@@ -131,9 +149,9 @@ def train(process_id, queue_to_master, queue_from_master, train_loader, test_loa
             history['test_acc'].append(test_acc)
             history['time'].append(end - start)
 
-    save_file(history, './saved/all_reduce/history_'+str(process_id)+'.pkl')
-    torch.save(model.state_dict(), './saved/all_reduce/weights_'+str(process_id)+'.pth')
-    return_queue.put((process_id, './saved/all_reduce/history_'+str(process_id)+'.pkl', './saved/all_reduce/weights_'+str(process_id)+'.pth'))
+    save_file(history, './saved/ring_all_reduce/history_'+str(process_id)+'.pkl')
+    torch.save(model.state_dict(), './saved/ring_all_reduce/weights_'+str(process_id)+'.pth')
+    return_queue.put((process_id, './saved/ring_all_reduce/history_'+str(process_id)+'.pkl', './saved/ring_all_reduce/weights_'+str(process_id)+'.pth'))
 
 def split_dataset(dataset, k=NUM_PROCESS):
     total_size = len(dataset)
@@ -153,7 +171,7 @@ def make_plot(train_values, test_values, x_label, y_label, legends, title, file_
     pyplot.legend(legends)
     pyplot.title(title)
     pyplot.grid(True)
-    pyplot.savefig('./saved/all_reduce/' + file_name)
+    pyplot.savefig('./saved/ring_all_reduce/' + file_name)
     pyplot.close()
 
 def main():
@@ -171,18 +189,17 @@ def main():
 
     mp.set_start_method('spawn', force=True)
 
-    queue_to_master = mp.Queue()
-    queue_from_master = mp.Queue()
+    queues = [mp.Queue() for i in range(NUM_PROCESS)]
     metrics_queue = mp.Queue()
     return_queue = mp.Queue()
 
     model = LeNet5(num_classes=10, conv_type='normal')
-
+    
     processes = []
     for i in range(NUM_PROCESS):
         p = mp.Process(
             target=train,
-            args=(i, queue_to_master, queue_from_master, train_loaders[i], test_loader, model.state_dict(), 100, metrics_queue, DEVICES[i], return_queue)
+            args=(i, queues, train_loaders[i], test_loader, model.state_dict(), 100, metrics_queue, DEVICES[i], return_queue)
         )
         processes.append(p)
         p.start()
@@ -202,16 +219,16 @@ def main():
     for i in range(NUM_PROCESS):
         if results[i][0] == 0:
             history = results[i][1]
-            torch.save(results[i][2], './saved/all_reduce/model.pth')
-            save_file(history, './saved/all_reduce/history.pkl')
+            torch.save(results[i][2], './saved/ring_all_reduce/model_ring.pth')
+            save_file(history, './saved/ring_all_reduce/history_ring.pkl')
 
-    print("Average Time taken to train one epoch of the model using ALL Reduce :", sum(history['time']) / len(history['time']))
+    print("Average Time taken to train one epoch of the model using Ring ALL Reduce :", sum(history['time']) / len(history['time']))
 
     legends = ['Training Loss Convolution', 'Test Loss Convolution']
-    make_plot(history['train_loss'], history['test_loss'], 'epoch', 'loss', legends, 'Loss vs Number of Epochs', 'Normal_Loss_vs_Epoch.png')
+    make_plot(history['train_loss'], history['test_loss'], 'epoch', 'loss', legends, 'Loss vs Number of Epochs', 'Ring_Loss_vs_Epoch.png')
 
     legends = ['Training Accuracy Convolution', 'Test Accuracy Convolution']
-    make_plot(history['train_acc'], history['test_acc'], 'epoch', 'accuracy', legends, 'Accuracy vs Number of Epochs', 'Normal_Accuracy_vs_Epoch.png')
+    make_plot(history['train_acc'], history['test_acc'], 'epoch', 'accuracy', legends, 'Accuracy vs Number of Epochs', 'Ring_Accuracy_vs_Epoch.png')
 
 if __name__ == "__main__":
     main()
